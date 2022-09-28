16e3aea1c9c78e18053562da6063e919f4339bde
taboh delarge <tdelarge8@gmail.com>
2022-09-28 13:52:01 +0200

    Update nexus-installation6.sh

--- nexus-installation/nexus-installation6.sh
+++ nexus-installation/nexus-installation6.sh
@@ -17,7 +17,7 @@ cd /opt
 
 # 1.Install prerequisit: JAVA, git, unzip
 
-sudo yum install wget git nano unzip -y
+sudo yum install wget vim git nano unzip -y
 sudo yum install java-11-openjdk-devel java-1.8.0-openjdk-devel -y
 
 
